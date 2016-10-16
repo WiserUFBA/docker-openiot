674f77e21bda392997acce499ed684c37cc037e1
jefersonla <oi_je@hotmail.com>
2016-10-16 13:51:42 -0300

    Changed the way virtuoso_config works to avoid variable errors

--- virtuoso_config.sh
+++ virtuoso_config.sh
@@ -4,11 +4,14 @@
 # <odifica a senha do usuaro dba para a variavel de ambiente
 # Adiciona a role SPARQL_UPDATE para o usuario SPARQL
 # Cria os gráficos necessários 
-isql -U dba -P dba <<EOT
+cat > /tmp/virtuoso_dba <<EOF
 SET PASSWORD dba $VIRTUOSO_DBA_PASS;
 USER_GRANT_ROLE('SPARQL', 'SPARQL_UPDATE', 0);
 SPARQL
     CREATE GRAPH <http://lsm.deri.ie/OpenIoT/sensormeta#>
     CREATE GRAPH <http://lsm.deri.ie/OpenIoT/sensordata#>
     CREATE GRAPH <http://lsm.deri.ie/OpenIoT/functionaldata#>;
-EOT
+EOF
+
+# Executa a query
+isql -U dba -P dba < /tmp/virtuoso_dba
